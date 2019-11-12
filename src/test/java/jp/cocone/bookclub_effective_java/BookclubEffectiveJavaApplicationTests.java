63e3bb5b5b6c0d728e8cb35430d4d21e78e05786
崔 仁惠 <choi_inhye@cocone.co.jp>
2019-11-12 17:40:07 +0900

    Initial commit

---
+++ src/test/java/jp/cocone/bookclub_effective_java/BookclubEffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.cocone.bookclub_effective_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookclubEffectiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
